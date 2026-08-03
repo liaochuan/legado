9b1171925b76f67015b3ea97d011db931e436225
mgz0227 <69211489+mgz0227@users.noreply.github.com>
2026-08-03 20:11:28 +0000

    Bump web v3.26.080404

--- modules/web/src/components.d.ts
+++ modules/web/src/components.d.ts
@@ -28,6 +28,7 @@ declare module 'vue' {
     ElTag: typeof import('element-plus/es')['ElTag']
     ElText: typeof import('element-plus/es')['ElText']
     ElTooltip: typeof import('element-plus/es')['ElTooltip']
+    LegacyReviewDialog: typeof import('./components/LegacyReviewDialog.vue')['default']
     PopCatalog: typeof import('./components/PopCatalog.vue')['default']
     ReadSettings: typeof import('./components/ReadSettings.vue')['default']
     ReviewDialog: typeof import('./components/ReviewDialog.vue')['default']
