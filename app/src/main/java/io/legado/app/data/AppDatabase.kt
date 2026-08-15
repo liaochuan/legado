8254dd3165d891d24fd2de7ae017983baa3be5a6
miaogongzi <miaogongzi0227@gmail.com>
2026-08-15 16:08:55 +0800

    文件夹分组显示书籍封面预览 (#757)

--- app/src/main/java/io/legado/app/data/AppDatabase.kt
+++ app/src/main/java/io/legado/app/data/AppDatabase.kt
@@ -175,6 +175,7 @@ abstract class AppDatabase : RoomDatabase() {
         const val DATABASE_NAME = "legado.db"
 
         const val BOOK_TABLE_NAME = "books"
+        const val BOOK_GROUP_TABLE_NAME = "book_groups"
         const val BOOK_SOURCE_TABLE_NAME = "book_sources"
         const val RSS_SOURCE_TABLE_NAME = "rssSources"
 
